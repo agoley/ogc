f62527616e291b7412b58e5799267de0bcdd6c59
ajgoley@gmail.com <ajgoley@gmail.com>
2015-11-17 01:37:28 -0500

    test

--- user_services.js
+++ user_services.js
@@ -4,14 +4,14 @@ var User = mongoose.model('User');
 var Transaction = mongoose.model('Transaction');
 var crypto = require('crypto');
 var sha = require('sha256');
-/*function hashPW(pwd){
+function hashPW(pwd){
    return crypto.createHash('sha256').update(pwd).
           digest('base64').toString();
 };
-*/
-function hashPW(pwd) {
+
+/*function hashPW(pwd) {
 	return sha(pwd);
-};
+};*/
 
 exports.removeFromCart = function(req, res) {
 	console.log(req.body);
