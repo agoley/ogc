54f4d2176f8d7715f6a03bb9e29a584c368d3b26
ajgoley@gmail.com <ajgoley@gmail.com>
2015-11-16 22:56:22 -0500

    allow origin

--- node_server.js
+++ node_server.js
@@ -66,7 +66,7 @@ app.use(expressSession({
 	})
 }));
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://agile-shelf-4123.herokuapp.com/");
+    res.header("Access-Control-Allow-Origin", "https://agile-shelf-4123.herokuapp.com/");
     res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
     if ('OPTIONS' == req.method){
