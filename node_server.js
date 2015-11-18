5d5f1690e113e4054141ed3f5855ecd177937c08
ajgoley@gmail.com <ajgoley@gmail.com>
2015-11-17 23:33:19 -0500

    test

--- node_server.js
+++ node_server.js
@@ -74,7 +74,7 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
-    if ('OPTIONS' == req.method){
+    if ('OPTIONS' === req.method){
         return res.sendStatus(200);
     }
     next();
