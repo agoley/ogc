8e60d4f3724ab3e0277b4956d3165842281c8b9e
ajgoley@gmail.com <ajgoley@gmail.com>
2015-11-17 23:41:02 -0500

    test

--- node_server.js
+++ node_server.js
@@ -72,9 +72,10 @@ app.use(expressSession({
 }));
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
-    if (req.method == 'OPTIONS'){
+    if ('OPTIONS' == req.method){
         return res.sendStatus(200);
     }
     next();
