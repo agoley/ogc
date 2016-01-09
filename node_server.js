7c3b9dc6b009c07fc1b3f5972c26a64b7174f3fe
ajgoley@gmail.com <ajgoley@gmail.com>
2016-01-09 13:11:23 -0500

    change request endpoint

--- node_server.js
+++ node_server.js
@@ -74,7 +74,7 @@ app.post('/api/photo',function(req,res){
 });
 
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://www.onlinegamecash.com' );
+    res.header('Access-Control-Allow-Origin', '*' );
 	res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With','X-Custom-Header');
