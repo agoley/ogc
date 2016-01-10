9fab077cf15bb5cd4b314543b7c7aa6bdee31aab
ajgoley@gmail.com <ajgoley@gmail.com>
2016-01-09 19:28:51 -0500

    change request endpoint

--- node_server.js
+++ node_server.js
@@ -85,7 +85,7 @@ app.all('*', function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.send(200);
+      next();
     }
     res.header('Access-Control-Allow-Origin', '*' );
 	res.header('Access-Control-Allow-Credentials', true);
