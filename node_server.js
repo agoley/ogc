b741037073cb577af780dab661e7da1828d1e5cd
ajgoley@gmail.com <ajgoley@gmail.com>
2016-01-09 19:30:11 -0500

    change request endpoint

--- node_server.js
+++ node_server.js
@@ -81,12 +81,6 @@ app.use(allowCrossDomain);
 
 //cors and preflight filtering 
 app.all('*', function(req, res, next) {
-	if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      next();
-    }
     res.header('Access-Control-Allow-Origin', '*' );
 	res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
