1fe3d7c80ec2bdf4cb43b2f73aa9341c4e70669b
ajgoley@gmail.com <ajgoley@gmail.com>
2016-01-11 19:00:36 -0500

    log req test

--- user_services.js
+++ user_services.js
@@ -307,6 +307,7 @@ exports.signup = function(req, res){
 
  exports.signin = function(req, res){
 	var postReq = JSON.parse(Object.keys(req.body));
+	console.log(postReq);
    User.findOne({ email: postReq.email })
    .exec(function(err, user) {
      if (!user){
