153f977310494f518ee0db5ccd1ebda6c409af66
ajgoley@gmail.com <ajgoley@gmail.com>
2016-01-13 22:08:27 -0500

    log req test

--- static/scripts/controllers/main.js
+++ static/scripts/controllers/main.js
@@ -595,7 +595,7 @@ app.controller('LoginController', ['$scope', '$http', function($scope, $http ) {
 	$scope.signin2 = function(){
 		if($scope.credentials.email && $scope.credentials.password){
 			$http.post('//agile-shelf-4123.herokuapp.com/signin2',  $scope.credentials).success(function(data, status, headers, config) {
-				//console.log(data);
+				console.log(data);
 				$scope.authenticated = true;
 			}).error(function(data, status, headers, config) {
 				console.log("App failed to post to //agile-shelf-4123.herokuapp.com/signup");
