49544ed672aed7b50548dde3042a564c81d54239
ajgoley@gmail.com <ajgoley@gmail.com>
2016-01-19 23:28:16 -0500

    test

--- node_server.js
+++ node_server.js
@@ -22,7 +22,7 @@ require('./users_model.js');
 require('./games_model.js');
 require('./transaction_model.js');
 
-//cors and preflight filtering 
+/*(cors and preflight filtering 
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*' );
 	res.header('Access-Control-Allow-Credentials', true);
@@ -31,7 +31,26 @@ app.all('*', function(req, res, next) {
     /*if ('OPTIONS' == req.method){
 		res.header('Access-Control-Allow-Origin', '*' );
         return res.sendStatus(200);
-    }*/
+    }*
+    next();
+});*/
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
 });
 app.use(bodyParser());
