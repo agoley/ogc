952fee14243ee4d57b0b1026821cb2d3408476b4
ajgoley@gmail.com <ajgoley@gmail.com>
2016-01-19 23:45:29 -0500

    test

--- node_server.js
+++ node_server.js
@@ -30,8 +30,9 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     if (req.method === 'OPTIONS'){
         return res.sendStatus(200);
-    }
-    next();
+    } else {
+		next();
+	}
 });
 app.use(bodyParser());
 app.use(cookieParser());
