73babdffe6794a9f2de30a19de84d3848616dd85
ajgoley@gmail.com <ajgoley@gmail.com>
2016-01-30 17:02:52 -0500

    ongoing dev

--- node_server.js
+++ node_server.js
@@ -7,7 +7,18 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
-app.use(cors());
+//app.use(cors());
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*' );
+	res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if (req.method === 'OPTIONS'){
+        res.send(200);
+		next();
+    } 
+	next();
+});
 var multer  = require('multer');
 var bodyParser = require('body-parser');
 app.engine('.html', require('ejs').__express);
