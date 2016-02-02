e3fffda77c5aab91c500c8fe024b512f7bce93c2
ajgoley@gmail.com <ajgoley@gmail.com>
2016-02-01 20:04:55 -0500

    ongoing dev

--- node_server.js
+++ node_server.js
@@ -6,8 +6,19 @@
 //var app = express();
 var express = require('express');
 var app = express();
-var cors = require('cors');
-app.use(cors());
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*' );
+	res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if (req.method === 'OPTIONS'){
+        res.send(200);
+    } else {
+		next();
+	}
+});
+//var cors = require('cors');
+//app.use(cors());
 /*var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
