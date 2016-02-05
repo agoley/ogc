1cd0a5638ccbeebaae405b4124571d64103417a9
ajgoley@gmail.com <ajgoley@gmail.com>
2016-02-04 19:32:27 -0500

    ongoing dev

--- node_server.js
+++ node_server.js
@@ -57,6 +57,7 @@ require('./transaction_model.js');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 app.use(session({
+	secret: 'SECRET',
     saveUninitialized: false, // don't create session until something stored
     resave: false, //don't save session if unmodified
     store: new MongoStore({ 
