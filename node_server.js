50039f617c60e1ed12fbbcfb6281b7f83a4fe926
ajgoley@gmail.com <ajgoley@gmail.com>
2016-02-04 19:44:44 -0500

    ongoing dev

--- node_server.js
+++ node_server.js
@@ -61,8 +61,7 @@ var MongoStore = require('connect-mongo')(session);
 app.use(session({
 	secret: 'foo',
 	cookie  : {
-      expires: false,
-      domain: config.cookie.domain
+      expires: false
     },
     saveUninitialized: false, // don't create session until something stored
     resave: false, //don't save session if unmodified
