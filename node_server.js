8dc4ecad39ec48768c875157d3f35a0601b4a026
ajgoley@gmail.com <ajgoley@gmail.com>
2016-02-06 16:19:56 -0500

    dev

--- node_server.js
+++ node_server.js
@@ -60,7 +60,7 @@ var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 app.use(session({
 	secret: 'foo',
-    cookie: {domain: 'onlinegamecash.com', httpOnly: false, secure: false, maxAge: null },
+    //cookie: {domain: 'onlinegamecash.com', httpOnly: false, secure: false, maxAge: null },
     saveUninitialized: false, // don't create session until something stored
     resave: false, //don't save session if unmodified
     store: new MongoStore({ 
