7ae2d83b0ff5a02277a3562fc94d4fc7fbd29b95
ajgoley@gmail.com <ajgoley@gmail.com>
2016-02-09 16:29:08 -0500

    dev

--- user_services.js
+++ user_services.js
@@ -287,7 +287,8 @@ exports.coinUser = function(req, res) {
 }
 
 exports.signout = function(req, res) {
-    res.clearCookie('connect.sid', { path: '/' }); // see comments above                res.send('removed session', 200); // tell the client everything went well
+    res.clearCookie('connect.sid', { path: '/' }); 
+    res.send('removed session', 200); // tell the client everything went well
 };
 	
 //sign up	
