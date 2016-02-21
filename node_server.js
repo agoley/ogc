5ecc6326a1ce5b3494b019ba738bee781a2e99c1
ajgoley@gmail.com <ajgoley@gmail.com>
2016-02-20 19:34:41 -0500

    port to 5000

--- node_server.js
+++ node_server.js
@@ -243,5 +243,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 require('./routes')(app);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port);
